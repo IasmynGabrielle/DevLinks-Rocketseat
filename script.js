65d94c6adf16e52026d3f143603d62d69aba36b8
IasmynGabrielle <iasmyngabriellee@gmail.com>
2023-02-03 22:31:37 -0300

    Add README

--- script.js
+++ script.js
@@ -15,15 +15,10 @@ function toggleMode() {
   if(html.classList.contains('light')) {
     //se tiver light mode, adicionar a imagem light (avatar)
     img.setAttribute("src", "./assets/Avatar.png")
+    alt.setAttribute("alt", "Avatar de Iasmyn Gabrielle, cabelos loiros e olhos azuis, usando uma blusa verde-água e fundo marrom.")
   } else {
     // se estiver sem light mode, manter a imagem normal
     img.setAttribute("src", "./assets/avatar.2.jpg")
-  }
-
-  // desafio: novo atributo alt
-  if(html.classList.contains('light')) {
-    alt.setAttribute("alt", "Avatar de Iasmyn Gabrielle, cabelos loiros e olhos azuis, usando uma blusa verde-água e fundo marrom.")
-  } else {
     alt.setAttribute("alt", "Avatar de Iasmyn Gabrielle, cabelos loiros escuros e olhos azuis, usando uma blusa verde-água e fundo bege.")
-  }  
+  } 
 }
